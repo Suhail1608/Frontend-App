d8833e8b4b060007cad6120bfde0f8bb7c51ea57
Suhail1608 <suhailbasha1608@gmail.com>
2024-09-13 11:44:51 +0530

    first commit

---
+++ frontend/src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {ToastContainer} from 'react-toastify'
+createRoot(document.getElementById('root')).render(
+    <ToastContainer autoClose={1000} hideProgressBar={true} > 
+      <App />
+    </ToastContainer>,
+)
