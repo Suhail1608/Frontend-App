0589e84c179cb3e018a54a9487f85518286eb9c4
Suhail1608 <suhailbasha1608@gmail.com>
2024-09-13 13:07:10 +0530

    cleanup

---
+++ frontend/src/config/toast.js
@@ -0,0 +1,12 @@
+export const toastContainerProps = {
+    rtl: false,
+    autoClose: 1000,
+    hideProgressBar: true,
+    style: { textAlign: 'left' },
+    toastStyle: {
+      position: 'absolute',
+      opacity: 1,
+      right: 0,
+      minWidth: '300px'
+    },
+  };
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,13 +2,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {ToastContainer} from 'react-toastify'
+import { toastContainerProps } from './config/toast.js';
 import 'react-toastify/dist/ReactToastify.css';
 import { Provider } from 'react-redux';
 import store from './redux/store.js';
 createRoot(document.getElementById('root')).render(
    
       <Provider store={store}>
-        <ToastContainer rtl={false} autoClose={1000} hideProgressBar style={{textAlign:'left'}} stacked={true} toastStyle={{position:'absolute',transform:'none',top:0}}/>
+        <ToastContainer {...toastContainerProps}/>
         <App />
       </Provider>
     ,
